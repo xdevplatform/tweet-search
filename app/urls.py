fc3524b5ebf8eada91f9f9f1ae400515193e5ca5
Ryan Choi <ryan.choi@gmail.com>
2014-11-24 13:00:58 -0800

    minor tweaks

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic.base import TemplateView, RedirectView
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +9,9 @@ urlpatterns = patterns('',
 
     url(r'^$', 'home.views.home', name='home'),
     url(r'^logout$', 'home.views.logout', name='logout'),
+
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/bird.ico')),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')),
 
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
