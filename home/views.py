cebe8c8ca166d4397ece64abe1235521547e37f5
Ryan Choi <ryan.choi@gmail.com>
2014-12-03 09:39:37 -0800

    filtering better

--- home/views.py
+++ home/views.py
@@ -59,7 +59,7 @@ def home(request):
 #         print g.query_api(query, query=True)
 
         # counts over time
-        query_nrt = query + " -(is:retweet)"
+        query_nrt = query
         timeline = g.query_api(query_nrt, 0, "timeline")
         count = 0
         for t in timeline:
@@ -74,39 +74,79 @@ def home(request):
     tweets = request.REQUEST.get("tweets", "")
     if query and tweets: 
 
+        queryCount = int(request.REQUEST.get("queryCount", TWEET_QUERY_COUNT))
+        followersCount = int(request.REQUEST.get("followersCount", 0))
+        friendsCount = int(request.REQUEST.get("friendsCount", 0))
+        statusesCount = int(request.REQUEST.get("statusesCount", 0))
+        favoritesCount = int(request.REQUEST.get("favoritesCount", 0))
+        retweets = int(request.REQUEST.get("retweets", 0))
         english = request.REQUEST.get("english", 0)
+        klout_score = request.REQUEST.get("klout_score", 0)
+
+        query_nrt = query
+
+#         if followersCount:
+#             query_nrt = query_nrt + " (followers_count:%s)" % followersCount
+#         if friendsCount:
+#             query_nrt = query_nrt + " (friends_count:%s)" % friendsCount
+#         if statusesCount:
+#             query_nrt = query_nrt + " (statuses_count:%s)" % statusesCount
+#         if favoritesCount:
+#             query_nrt = query_nrt + " (favorites_count:%s)" % favoritesCount
+        if not retweets:
+            query_nrt = query_nrt + " -(is:retweet)"
         if english:
             query_nrt = query_nrt + " (lang:en)" 
-    
-        klout_score = request.REQUEST.get("klout_score", 0)
         if klout_score:
             query_nrt = query_nrt + " klout_score:%s" % klout_score
-            
-        followers_count = request.REQUEST.get("followers_count", 0)
-        if followers_count:
-            query_nrt = query_nrt + " followers_count:%s" % followers_count
-            
-        friends_count = request.REQUEST.get("friends_count", 0)
-        if friends_count:
-            query_nrt = query_nrt + " friends_count:%s" % friends_count
+    
+        print "%s (%s)" % (query_nrt, queryCount)
     
         # last N tweets, categorized by sentiment
         top = []
         bottom = []
-        tweets = g.query_api(query_nrt, TWEET_QUERY_COUNT)
+        tweets = []
+        tc = 0
+        
+        if queryCount > 500:
+            g.paged = True
+            
+        while tc < queryCount:
+            tweets_cursor = g.query_api(query_nrt, queryCount)
+            tweets = tweets + tweets_cursor
+            tc = len(tweets)
+            print "query paging: %s " % tc  
         positive = 0
         neutral = 0
         negative = 0
-        count = len(tweets)
+        count = 0
 
-        for i in range(count):
+        for i in range(len(tweets)):
             
             tweet = json.loads(tweets[i])
             tweets[i] = tweet
             
+            if followersCount and tweet["actor"]["followersCount"] < followersCount:
+                print "pass followersCount %s" % tweet["actor"]["followersCount"]
+                continue
+
+            if friendsCount and tweet["actor"]["friendsCount"] < friendsCount:
+                print "pass friendsCount %s" % tweet["actor"]["friendsCount"]
+                continue
+
+            if statusesCount and tweet["actor"]["statusesCount"] < statusesCount:
+                print "pass statusesCount %s" % tweet["actor"]["statusesCount"]
+                continue
+
+            if favoritesCount and tweet["actor"]["favoritesCount"] < favoritesCount:
+                print "pass favoritesCount %s" % tweet["actor"]["favoritesCount"]
+                continue
+            
             body = tweet["body"]
             response = get_sentiment(query, body)
             if response:
+                
+                count = count + 1
 
                 # top-level sentiment                
                 label = response["label"]
@@ -125,10 +165,8 @@ def home(request):
                         top.append(tweet)
                     elif label == "neg":
                         bottom.append(tweet)
-#                     else:
-#                         neutral = neutral + 1
                         
-                print label, sentiment, body
+#                 print label, sentiment, body, json.dumps(tweet)
             
         top = sorted(top, key=lambda t: -t["sentiment"])
         if len(top) > 10:
@@ -140,9 +178,9 @@ def home(request):
         
         context["tweets"] = {
              "count": count,
-             "positive": int(positive*100/count),
-             "neutral": int(neutral*100/count),
-             "negative": int(negative*100/count),
+             "positive": int(positive*100/count) if count > 0 else 0,
+             "neutral": int(neutral*100/count) if count > 0 else 0,
+             "negative": int(negative*100/count) if count > 0 else 0,
              "top" : top,
              "bottom": bottom
          }
@@ -160,6 +198,7 @@ def home(request):
        "count" : len(tweets)
     }
     context["results"] = results
+    context["queryCount"] = TWEET_QUERY_COUNT
     
     return render_to_response('home.html', context, context_instance=RequestContext(request))
 
