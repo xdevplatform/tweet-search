8fb4a32158468e2d226501ba069cc3bbbf7d603a
Ryan Choi <ryan.choi@gmail.com>
2015-03-18 13:13:08 -0700

    static

--- app/settings.py
+++ app/settings.py
@@ -110,7 +110,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
