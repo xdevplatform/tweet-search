eaaee11234c1e70985cc9aa3a1dcafb9c49da984
Ryan Choi <ryan.choi@gmail.com>
2015-03-18 12:55:39 -0700

    static files

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     'social.apps.django_app.default',
     'south',
-    'django_verbatim',
     'tags',
     'app',
     'home',
@@ -111,6 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
