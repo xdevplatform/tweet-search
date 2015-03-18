56ba7c8523793bb53fa10e463ce4154c4f5a4490
Ryan Choi <ryan.choi@gmail.com>
2015-03-18 12:07:17 -0700

    heroku backend

--- app/settings.py
+++ app/settings.py
@@ -83,11 +83,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# Uncomment for Heroku
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite://tweet-search.db')
 }
 
 # Internationalization
