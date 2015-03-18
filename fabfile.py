084eda2b47a2a1497970f6bc184da9fe4a85700c
Ryan Choi <ryan.choi@gmail.com>
2015-03-18 12:44:53 -0700

    port change

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,13 @@ from __future__ import with_statement
 
 from fabric.api import *
 from fabric.contrib import django
+import os
 
 SETTINGS_FILE = "app.settings_my"
 django.settings_module(SETTINGS_FILE)
-
+PORT = os.environ.get('PORT', 9000)
 
 # run server locally
 def start():
-    local("python manage.py runserver 127.0.0.1:9000 --traceback --settings=%s" % SETTINGS_FILE)
-
+    local("python manage.py runserver 127.0.0.1:%s --traceback --settings=%s" % (PORT, SETTINGS_FILE))
 
