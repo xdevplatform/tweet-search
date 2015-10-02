e721cb08c791ec0966e20c162230bfbc8f862728
Ryan Choi <ryan.choi@gmail.com>
2015-10-02 14:32:23 -0700

    admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 # Register your models here.
+
+UserAdmin.list_display = ('first_name', 'last_name', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
