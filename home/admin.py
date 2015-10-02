404997c8b11e5012aa3b710f445577b554555395
Ryan Choi <ryan.choi@gmail.com>
2015-10-02 14:34:50 -0700

    adding username

--- home/admin.py
+++ home/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 # Register your models here.
 
-UserAdmin.list_display = ('first_name', 'last_name', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
+UserAdmin.list_display = ('first_name', 'last_name', 'username', 'date_joined', 'is_staff', 'is_superuser')
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
