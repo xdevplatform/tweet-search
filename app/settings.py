096e15e40453f615b5f42df5c0033c57d619226b
Ryan Choi <ryan.choi@gmail.com>
2016-01-29 12:09:26 -0800

    csp

--- app/settings.py
+++ app/settings.py
@@ -138,11 +138,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # security: https://django-csp.readthedocs.org/en/latest/configuration.html#policy-settings
 CSP_DEFAULT_SRC = ("'self'", 'https://*.twitter.com',)
-CSP_IMG_SRC = ("'self'", 'https://www.google-analytics.com', )
-CSP_SCRIPT_SRC = ("'self'", 'https://www.google-analytics.com', 'https://ajax.googleapis.com', 'https://maxcdn.bootstrapcdn.com', )
-CSP_FRAME_SRC = ("'self'", )
-CSP_STYLE_SRC = ("'self'", 'https://fonts.googleapis.com', 'https://maxcdn.bootstrapcdn.com', )
-CSP_FONT_SRC = ("'self'", 'https://fonts.gstatic.com', 'https://maxcdn.bootstrapcdn.com', )
+CSP_IMG_SRC = ("'self'", 'https://*.twitter.com', 'https://www.google-analytics.com', )
+CSP_SCRIPT_SRC = ("'self'", 'https://*.twitter.com', 'https://www.google-analytics.com', 'https://ajax.googleapis.com', 'https://maxcdn.bootstrapcdn.com', )
+CSP_FRAME_SRC = ("'self'", 'https://*.twitter.com', )
+CSP_STYLE_SRC = ("'self'", 'https://*.twitter.com', 'https://fonts.googleapis.com', 'https://maxcdn.bootstrapcdn.com', )
+CSP_FONT_SRC = ("'self'", 'https://*.twitter.com', 'https://fonts.gstatic.com', 'https://maxcdn.bootstrapcdn.com', )
 CSP_OBJECT_SRC = ("'none'", )
 
 SOCIAL_AUTH_LOGIN_URL          = '/login'
